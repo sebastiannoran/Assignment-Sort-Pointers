982266b945f5e51928070c0f5071ad8aa9999033
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-07 15:28:19 -0400

    Soltion A.1 complete

--- Set-A/findMissNum.js
+++ Set-A/findMissNum.js
@@ -1,14 +1,25 @@
 function missingNumber (nums) {
-    for (let currIndex = 0; currIndex < nums.length; currIndex++) {
-        const correctIndex = nums[currIndex] - 1;
-        if (currIndex !== correctIndex) {
-            return nums[correctIndex];
+    let n = nums.length // record the range of the array
+    for (let currIndex = 0; currIndex < n; currIndex++) {
+        const correctIndex = nums[currIndex]
+        if (nums[currIndex] !== nums[correctIndex]) {
+            [nums[currIndex], nums[correctIndex]] = [nums[correctIndex], nums[currIndex]];
+        }
+    }
+    // with array sorted using cyclic sort, find the missing num using additional for loop
+    // still keep 0(n) time
+    for (let i = 0; i <= n; i++) {
+        if (nums[i] !== i) {
+            return i;
         }
     }
 }
 
 let nums = [3,0,1];
 
-console.log(missingNumber(nums));
+const missingNum = missingNumber(nums);
+
+console.log(missingNum);
+
 
 module.exports = missingNumber;
