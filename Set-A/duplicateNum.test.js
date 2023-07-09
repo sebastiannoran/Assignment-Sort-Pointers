fc7e43e6f6bc6b42b865380fc7c9f0049b1dbde3
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-09 13:51:10 -0400

    final test case complete

--- Set-A/duplicateNum.test.js
+++ Set-A/duplicateNum.test.js
@@ -4,4 +4,8 @@ describe("duplicateNum(nums) should read in an array and output a found duplicat
     test("duplicateNum([1,3,4,2,2]) should return 2", () => {
         expect(duplicateNum([1,3,4,2,2])).toEqual(2);
     })
+
+    test("duplicateNum([3,1,3,4,2] should return 3", () => {
+        expect(duplicateNum([3,1,3,4,2])).toEqual(3);
+    })
 })
