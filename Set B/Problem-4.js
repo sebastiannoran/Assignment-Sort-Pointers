a9e93a664e61475e81c9ba9b1a6760f9ea1b072c
Samr Mouna <samr.mouna@gmail.com>
2023-07-09 18:15:10 -0400

    used a while loop instead to better control when our current index increases

--- Set B/Problem-4.js
+++ Set B/Problem-4.js
@@ -4,27 +4,32 @@ function findAllDuplicates(nums){
     // Beginning of Cyclic sort, getting indexes based on values
     let currentIndex = 0;
     let duplicates = [];
-      // Iterate throughout our entire nums array
-      for(currentIndex; currentIndex < nums.length; currentIndex++) {
+      // Iterate throughout our entire nums array, with a while loop instead
+    while(currentIndex < nums.length) {
         // Get the current value
-        const value = nums[currentIndex];
+      const value = nums[currentIndex];
         // Set the correct index for the value
-        const correctIndex = value - 1;
+      const correctIndex = value - 1;
         // Swap the value into the correct index
-        if(currentIndex != correctIndex) {
-            const valueAtCorrectIndex = nums[correctIndex];
-            nums[currentIndex] = valueAtCorrectIndex;
-            nums[correctIndex] = value; 
-        }
+  
+      if (value !== currentIndex + 1) {
+        if (value === nums[correctIndex]) {
+            // Found a duplicate, if it doesn't already exist, push it to the duplicate array
+          if (!duplicates.includes(value)) {
+            duplicates.push(value);
+          }
+          currentIndex++;
+        } else {
+            // Swap the numbers, the only case where our index does not increase, because we need to check our current value again
+          const valueAtCorrectIndex = nums[correctIndex];
+          nums[currentIndex] = valueAtCorrectIndex;
+          nums[correctIndex] = value; 
+        } 
+      } else {
+        currentIndex++;
       }
-
-      // For every value in the newly sorted array, push any number that does not match the correct index
-      for(let i = 0; i < nums.length; i++) {
-        if(i + 1 != nums[i]){
-            duplicates.push(nums[i]);
-        }
-      }
-      // Return the duplicate
+    }
+      // Return the duplicate array
       return duplicates;
     }
     
