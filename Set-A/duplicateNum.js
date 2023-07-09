a898bfa970fb79c8e590db2d1be4d0fb347809f3
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-09 13:49:08 -0400

    question 3 solved

--- Set-A/duplicateNum.js
+++ Set-A/duplicateNum.js
@@ -3,3 +3,24 @@
 //There is only one repeated number in nums, return this repeated number.
 
 //You must solve the problem without modifying the array nums and uses only constant extra space.
+// we could use slow and fast pointers, right? check if fast === slow
+// no we can't, different data strucure (linked list vs array)
+// use cylic sort
+function duplicateNum(nums) {
+    const n = nums.length;
+    for (let currIndex = 0; currIndex < n; currIndex++) {   
+        const correctIndex = nums[currIndex] - 1;
+        if (nums[currIndex] !== nums[correctIndex]) {
+            [nums[currIndex], nums[correctIndex]] = [nums[correctIndex], nums[currIndex]];
+            currIndex--;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return nums[i];
+        }
+    }
+    return -1;
+}
+
+module.exports = duplicateNum;
--- Set-A/duplicateNum.test.js
+++ Set-A/duplicateNum.test.js
@@ -1,6 +1,7 @@
+const duplicateNum = require ("./duplicateNum");
 
-//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-
-//There is only one repeated number in nums, return this repeated number.
-
-//You must solve the problem without modifying the array nums and uses only constant extra space.
+describe("duplicateNum(nums) should read in an array and output a found duplicate value", () => {
+    test("duplicateNum([1,3,4,2,2]) should return 2", () => {
+        expect(duplicateNum([1,3,4,2,2])).toEqual(2);
+    })
+})
