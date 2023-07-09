c2f71ec8326f35fdbc96144748979c6386c4a242
Samr Mouna <samr.mouna@gmail.com>
2023-07-09 18:15:33 -0400

    testing complete for problem 4

--- Set B/Problem-4.test.js
+++ Set B/Problem-4.test.js
@@ -1,7 +1,10 @@
 const findAllDuplicates = require("./Problem-4");
 
-describe(("Find the single value that has been duplicated in the array"), () => {
+describe(("Find all duplictaes in an array"), () => {
 
-    
+    test("[1, 2, 3, 4, 4, 5, 6] => [4]", () => expect(findAllDuplicates([1, 2, 3, 4, 4, 5, 6])).toEqual([4]));
+    test("[1, 1, 2, 3, 4, 4, 5, 5, 6] => [1, 4, 5]", () => expect(findAllDuplicates([1, 1, 2, 3, 4, 4, 5, 5, 6])).toEqual([1, 4, 5]));
+    test("[] => []", () => expect(findAllDuplicates([])).toEqual([]));
+    test("[1, 2, 3, 4, 5] => []", () => expect(findAllDuplicates([1, 2, 3, 4, 5])).toEqual([]));
 
 })
