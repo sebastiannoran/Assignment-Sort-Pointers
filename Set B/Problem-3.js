ed9d6f10ba9c811720bcd38b385264ab724c5288
Samr Mouna <samr.mouna@gmail.com>
2023-07-09 17:57:13 -0400

    problem 3 complete with testing

--- Set B/Problem-3.js
+++ Set B/Problem-3.js
@@ -2,30 +2,25 @@
 function findDuplicate(nums){
     // Print the initial array for basic test case
     // Beginning of Cyclic sort, getting indexes based on values
-    let currentIndex = 0;
-    let duplicate = 0;
-      // Iterate throughout our entire nums array
-      for(currentIndex; currentIndex < nums.length; currentIndex++) {
-        // Get the current value
-        const value = nums[currentIndex];
-        // Set the correct index for the value
-        const correctIndex = value - 1;
-        // If the number at the current index already exists at its intended index, but the current index is NOT the correct index,
-        // mark it as the duplicate. This means there is an already existing version of the duplicate in the correct place
-        if(currentIndex != correctIndex && nums[currentIndex] !== nums[correctIndex]) {
-            const valueAtCorrectIndex = nums[correctIndex];
-            nums[currentIndex] = valueAtCorrectIndex;
-            nums[correctIndex] = value;
-        }
-      
-        if(currentIndex !== correctIndex && nums[currentIndex] === nums[correctIndex]) {
-            duplicate = nums[currentIndex];
-            break;
-        }
-      }
-      // Return the duplicate
+  let currentIndex = 0;
 
-      return duplicate;
+  while (currentIndex < nums.length) {
+    const value = nums[currentIndex];
+    if (value !== currentIndex + 1) {
+      if (value === nums[value - 1]) {
+        // Found a duplicate
+        return value;
+      }
+      // Swap the numbers
+      [nums[currentIndex], nums[value - 1]] = [nums[value - 1], nums[currentIndex]];
+      } else {
+        currentIndex++;
+      }
     }
 
+      // If nothing is found, return 0
+    return 0;
+}
+
+    console.log(findDuplicate([3, 4, 5, 6, 1, 1, 2]));
     module.exports = findDuplicate;
--- Set B/Problem-3.test.js
+++ Set B/Problem-3.test.js
@@ -3,7 +3,7 @@ const findDuplicate = require("./Problem-3");
 describe(("Find the single value that has been duplicated in the array"), () => {
 
     test("[1, 2, 3, 4, 4, 5, 6] => 4", () => expect(findDuplicate([1, 2, 3, 4, 4, 5, 6])).toBe(4)); // Case with sorted array
-    test("[3, 4, 5, 6, 1, 1, 2] => 4", () => expect(findDuplicate([3, 4, 5, 6, 1, 1, 2])).toBe(1)); // Case out of order
+    test("[3, 4, 5, 6, 1, 1, 2] => 1", () => expect(findDuplicate([3, 4, 5, 6, 1, 1, 2])).toBe(1)); // Case out of order
     test("[] => 0", () => expect(findDuplicate([])).toBe(0));                                        // Empty array (0 implies no duplicate because array can only contain numbers above 1)
-    test("[3, 4, 4, 5, 6, 1, 2] => 4", () => expect(findDuplicate([3, 4, 4, 5, 6, 1, 2])).toBe(4));   // Should return first duplicate only
+    test("[3, 4, 4, 5, 6, 1, 2] => 4", () => expect(findDuplicate([3, 4, 4, 5, 6, 1, 2])).toBe(4));   // Should return 4
 })
